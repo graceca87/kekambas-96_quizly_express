5ffcd177674f0ba573898481f640acc0b3b9c1e8
Brian <bstanton773@gmail.com>
2022-09-07 11:45:59 -0500

    Add register template

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 });
 
+// Set the view engine to ejs
+app.set('view engine', 'ejs')
+// Update the location of the views folder that res.render uses
+app.set('views', path.join(__dirname, 'src/templates/views'))
 
 // Initialize routes
 const initRoutes = require('./src/routes');
